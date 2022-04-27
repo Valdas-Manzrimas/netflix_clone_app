227da5deecdc3cb12d27c981dee845ab6ac1d504
Valdas Manzrimas <vrajaviharidas@gmail.com>
2022-04-27 16:43:09 +0200

    Created initial express routes

--- server.js
+++ server.js
@@ -4,6 +4,12 @@ const app = express();
 
 app.get('/', (req, res) => res.send('API running...'));
 
+// Define routes
+app.use('/api/users', require('./routes/api/users'))
+app.use('/api/auth', require('./routes/api/auth'))
+app.use('/api/profiles', require('./routes/api/profiles'))
+app.use('/api/videos', require('./routes/api/videos'))
+
 const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
